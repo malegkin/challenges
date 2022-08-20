834fa5585f4c46aac7d471ea1901f9062b2d719d
malegin <malegkin@gmail.com>
2022-08-21 01:09:45 +0300

    imp: leetcode.2.add-two-numbers: reduced memory usage

--- leetcode/2-add-two-numbers.py
+++ leetcode/2-add-two-numbers.py
@@ -1,3 +1,4 @@
+# https://leetcode.com/problems/add-two-numbers/
 # You are given two non-empty linked lists representing two non-negative integers.
 # The digits are stored in reverse order, and each of their nodes contains a single digit.
 # Add the two numbers and return the sum as a linked list.
@@ -62,6 +63,33 @@ class NumberList:
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        _last = None
+        _begin = None
+        next_add = 0
+
+        while l1 is not None or l2 is not None:
+            val = (0 if l1 is None else l1.val) + (0 if l2 is None else l2.val) + next_add
+            next_add = val // 10
+            val = val % 10
+
+            if _last is None:
+                _last = ListNode(val)
+                _begin = _last
+            else:
+                _last.next = ListNode(val)
+                _last = _last.next
+
+            l1 = None if l1 is None else l1.next
+            l2 = None if l2 is None else l2.next
+
+        if next_add > 0:
+            _last.next = ListNode(next_add)
+
+        return _begin
+
+
+class SolutionByNumberList:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         out = int(NumberList(l1)) + int(NumberList(l2))
         return NumberList(out).get_begin()
 
@@ -70,6 +98,14 @@ class TestSolution(unittest.TestCase):
     def test_int_to_number_list_and_back_converter(self):
         for i in [0, 1, 12, 123456789]:
             self.assertEqual(i, int(NumberList(i)))
+
+    def test_solution(self):
+        for solution_class in [Solution, SolutionByNumberList]:
+            solution = solution_class()
+            for x, y, z in [[0, 0, 0], [1, 2, 3], [12, 34, 46], [123, 45, 168], [99, 1, 100]]:
+                self.assertEqual(z, int(NumberList(
+                    solution.addTwoNumbers(NumberList(x).get_begin(), NumberList(y).get_begin()))
+                ))
 
 
 if __name__ == 'main':
