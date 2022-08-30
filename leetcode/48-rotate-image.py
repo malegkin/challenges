7e331afcf1aa0ffde003221f7d140aa8a37e1e67
malegin <malegkin@gmail.com>
2022-08-31 00:38:56 +0300

    add: leetcode.48-rotate-image solution

---
+++ leetcode/48-rotate-image.py
@@ -0,0 +1,39 @@
+# https://leetcode.com/problems/rotate-image/
+# You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+#
+# You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+# Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+# Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+from typing import List
+import unittest
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+            rotate matrix == transpose + revers
+            Time Complexity: O(n)
+            Auxiliary Space: O(1)
+        """
+        # Transposing the matrix
+        for i in range(len(matrix)):
+            for j in range(i, len(matrix)):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        # Reversing the matrix columns
+        for i in range(len(matrix)):
+            for j in range(len(matrix)//2):
+                matrix[i][j], matrix[i][~j] = matrix[i][~j], matrix[i][j]
+
+
+class TestSolution(unittest.TestCase):
+    def test_solution(self):
+        solution = Solution()
+        for matrix, expected in [
+            [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],     [[7, 4, 1], [8, 5, 2], [9, 6, 3]]],
+            [[[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]],    [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]]
+        ]:
+            solution.rotate(matrix)
+            self.assertListEqual(matrix, expected)
