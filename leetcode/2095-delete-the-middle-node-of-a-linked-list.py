b26b83e0efdd0993ca5652069bd376a6276745e4
malegin <malegkin@gmail.com>
2022-10-14 23:30:37 +0300

    add: leetcode:2095 Delete the Middle Node of a Linked List

---
+++ leetcode/2095-delete-the-middle-node-of-a-linked-list.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+# You are given the head of a linked list. Delete the middle node, and return the head of the modified linked list.
+# The middle node of a linked list of size n is the ⌊n / 2⌋th node from the start using 0-based indexing, where ⌊x⌋
+# denotes the largest integer less than or equal to x.
+# For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2, respectively.
+# Constraints:
+#   The number of nodes in the list is in the range [1, 10**5].
+#   1 <= Node.val <= 10**5
+
+
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        node = head
+        length = 0
+        while node:
+            length += 1
+            node = node.next
+
+        if length < 2:
+            return None
+
+        node = head
+        for _ in range((length // 2) - 1):
+            node = node.next
+
+        node.next = None if node.next is None else node.next.next
+
+        return head
