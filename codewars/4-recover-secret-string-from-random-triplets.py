7ba1604cede61f17aa3e982899f20b0ac4cb80c6
malegin <malegkin@gmail.com>
2022-10-15 22:21:06 +0300

    improve: codewars:4k Recover a secret string from random triplets

--- codewars/4-recover-secret-string-from-random-triplets.py
+++ codewars/4-recover-secret-string-from-random-triplets.py
@@ -10,35 +10,24 @@
 
 import random
 import string
+import itertools
 from timeit import timeit
 from unittest import TestCase
-from collections import deque
+from collections import deque, defaultdict
 
 
 def recoverSecret(triplets, c_type=list):
-    secret = c_type()
-    for triplet in triplets:
-        [x, y, z] = triplet
+    letters = c_type(set([l for t in triplets for l in t]))
 
-        if x not in secret:
-            secret.insert(0, x)
+    for t in triplets * len(letters):
+        for i in range(len(t) - 1):
+            a, b = letters.index(t[i]), letters.index(t[i + 1])
+            if (a > b): letters[b], letters[a] = letters[a], letters[b]
 
-        if y not in secret:
-            secret.insert(secret.index(x) + 1, y)
-        if secret.index(y) < secret.index(x):
-            secret.remove(y)
-            secret.insert(secret.index(x), y)
-
-        if z not in secret:
-            secret.insert(secret.index(y) + 1, z)
-        if secret.index(z) < secret.index(y):
-            secret.remove(z)
-            secret.insert(secret.index(y), z)
-
-    return ''.join(secret)
+    return ''.join(letters)
 
 
-def recoverSecretFaster(triplets):
+def recoverSecretPopularButInvalidSolution(triplets, c_type=list):
     """ best solution from all submitted"""
     def fix(l, a, b):
         """let l.index(a) < l.index(b)"""
@@ -46,33 +35,49 @@ def recoverSecretFaster(triplets):
             l.remove(a)
             l.insert(l.index(b), a)
 
-    r = list(set([i for l in triplets for i in l]))
+    r = sorted(c_type(set([i for l in triplets for i in l])))
+
     for l in triplets:
-        fix(r, l[1], l[2])
         fix(r, l[0], l[1])
+        fix(r, l[1], l[2])
+
     return ''.join(r)
 
 
 class TestSolution(TestCase):
-    def test_solution(self):
+    def test_solution_like_codewars(self):
+        for solution in [recoverSecret, recoverSecretPopularButInvalidSolution]:
+                for secret, triplets in [
+                    ["whatisup",  [['t', 'u', 'p'], ['w', 'h', 'i'], ['t', 's', 'u'], ['a', 't', 's'], ['h', 'a', 'p'],
+                                   ['t', 'i', 's'], ['w', 'h', 's']]],
+                    ["whatisup",  [['t', 'u', 'p'], ['w', 'h', 'i'], ['t', 's', 'u'], ['a', 't', 's'], ['h', 'a', 'p'],
+                                   ['t', 'i', 's'], ['w', 'h', 's']]],
+                    ["mathisfun", [['t', 's', 'f'], ['a', 's', 'u'], ['m', 'a', 'f'], ['a', 'i', 'n'], ['s', 'u', 'n'],
+                                   ['m', 'f', 'u'], ['a', 't', 'h'], ['t', 'h', 'i'], ['h', 'i', 'f'], ['m', 'h', 'f'],
+                                   ['a', 'u', 'n'], ['m', 'a', 't'], ['f', 'u', 'n'], ['h', 's', 'n'], ['a', 'i', 's'],
+                                   ['m', 's', 'n'], ['m', 's', 'u']]],
+                    ["congrats", [['g', 'a', 's'], ['o', 'g', 's'], ['c', 'n', 't'], ['c', 'o', 'n'], ['a', 't', 's'],
+                                  ['g', 'r', 't'], ['r', 't', 's'], ['c', 'r', 'a'], ['g', 'a', 't'], ['n', 'g', 's'],
+                                  ['o', 'a', 's']]]
+                ]:
+                    print(f"run_test {solution.__name__} {secret}")
 
-        for c_type in [list, deque]:
-            for secret, triplets in [
-                ["whatisup",  [['t', 'u', 'p'], ['w', 'h', 'i'], ['t', 's', 'u'], ['a', 't', 's'], ['h', 'a', 'p'],
-                               ['t', 'i', 's'], ['w', 'h', 's']]],
-                ["mathisfun", [['t', 's', 'f'], ['a', 's', 'u'], ['m', 'a', 'f'], ['a', 'i', 'n'], ['s', 'u', 'n'],
-                               ['m', 'f', 'u'], ['a', 't', 'h'], ['t', 'h', 'i'], ['h', 'i', 'f'], ['m', 'h', 'f'],
-                               ['a', 'u', 'n'], ['m', 'a', 't'], ['f', 'u', 'n'], ['h', 's', 'n'], ['a', 'i', 's'],
-                               ['m', 's', 'n'], ['m', 's', 'u']]],
-                ["congrats", [['g', 'a', 's'], ['o', 'g', 's'], ['c', 'n', 't'], ['c', 'o', 'n'], ['a', 't', 's'],
-                              ['g', 'r', 't'], ['r', 't', 's'], ['c', 'r', 'a'], ['g', 'a', 't'], ['n', 'g', 's'],
-                              ['o', 'a', 's']]]
-            ]:
-                print(f"run_test {c_type.__name__} {secret}")
-                self.assertEqual(secret, recoverSecret(triplets, c_type))
+    def test_solution_all_permutations(self):
+        for solution in [recoverSecret, recoverSecretPopularButInvalidSolution]:
+                for secret, triplets in [
+                    ["whatisup",  [['t', 'u', 'p'], ['w', 'h', 'i'], ['t', 's', 'u'], ['a', 't', 's'], ['h', 'a', 'p'],
+                                   ['t', 'i', 's'], ['w', 'h', 's']]]
+                ]:
+                    print(f"run_test {solution.__name__}  {secret}")
+                    secrets = defaultdict(int)
+                    for pts in itertools.permutations(triplets):
+                        secrets[solution(triplets)] += 1
+
+                    self.assertEqual(1, len(secrets))
+                    self.assertEqual(secret, max(secrets, key=secrets.get))
 
     def test_list_vs_deque(self):
         triplets = [random.sample(string.ascii_lowercase, 3) for _ in range(1000)]
-        for c_type in [list, deque]:
-            print(f"{c_type.__name__} {timeit(lambda: recoverSecret(triplets, c_type), number=100):.3f}")
-        print(f"recoverSecretFaster: {timeit(lambda: recoverSecretFaster(triplets), number=100):.3f}")
+        for solution in [recoverSecret, recoverSecretPopularButInvalidSolution]:
+            for c_type in [list, deque]:
+                print(f"{solution.__name__}: {c_type.__name__} {timeit(lambda: solution(triplets, c_type), number=10):.3f}")
