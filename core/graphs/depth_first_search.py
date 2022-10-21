6f0554b86068b331958ec05551eb7888b669c53c
malegin <malegkin@gmail.com>
2022-10-21 21:27:39 +0300

    DFS Graph

---
+++ core/graphs/depth_first_search.py
@@ -0,0 +1,56 @@
+# Python3 program to print DFS traversal
+# from a given  graph
+from collections import defaultdict
+
+
+# This class represents a directed graph using
+# adjacency list representation
+
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def add_edge(self, u, v):
+        self.graph[u].append(v)
+
+    # A function used by DFS
+    def DFSUtil(self, v, visited):
+
+        # Mark the current node as visited
+        # and print it
+        visited.add(v)
+        print(v, end=' ')
+
+        # Recur for all the vertices
+        # adjacent to this vertex
+        for neighbour in self.graph[v]:
+            if neighbour not in visited:
+                self.DFSUtil(neighbour, visited)
+
+    # The function to do DFS traversal. It uses
+    # recursive DFSUtil()
+    def DFS(self, v):
+
+        # Create a set to store visited vertices
+        visited = set()
+
+        # Call the recursive helper function
+        # to print DFS traversal
+        self.DFSUtil(v, visited)
+
+
+# Driver code
+
+
+# Create a graph given
+# in the above diagram
+g = Graph()
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(1, 2)
+g.add_edge(2, 0)
+g.add_edge(2, 3)
+g.add_edge(3, 3)
+
+print("Following is DFS from (starting from vertex 2)")
+g.DFS(2)
